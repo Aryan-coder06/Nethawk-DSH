937523c21487eb62e226e7eb155daddd15f6d021
Aryan-coder06 <aryaniiitian06@gmail.com>
2025-07-05 23:34:20 +0530

    fix: added gunicorn to requirements and Wsgi logger

--- nethawk-backend/wsgi.py
+++ nethawk-backend/wsgi.py
@@ -2,7 +2,8 @@ from app import create_app, socketio
 
 app = create_app()
 
-if __name__ != "__main__":
-    # Render will start Gunicorn via CMD
-    # This is all you need
-    pass
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logging.debug("WSGI: Flask app created successfully.")
+
+# nothing else needed; Render will use Gunicorn
